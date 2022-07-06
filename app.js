7cc8da88a64cf67adf34b96a8ac1e75e98b7ab75
twwli <hello@theworldwelivein.co.uk>
2022-07-06 09:12:05 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import * as PIXI from "/pixi.js";
-import { KawaseBlurFilter } from "@pixi/filter-kawase-blur";
+import { KawaseBlurFilter } from "/pixi/filter-kawase-blur";
 import SimplexNoise from "simplex-noise";
 import hsl from "hsl-to-hex";
 import debounce from "debounce";
