889459a22e6c4debd66756e0c4f0ed46a1e9ffee
twwli <hello@theworldwelivein.co.uk>
2022-07-06 09:10:30 +0200

    Change Pixi Import

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import * as PIXI from "pixi.js";
+import * as PIXI from "/pixi.js";
 import { KawaseBlurFilter } from "@pixi/filter-kawase-blur";
 import SimplexNoise from "simplex-noise";
 import hsl from "hsl-to-hex";
